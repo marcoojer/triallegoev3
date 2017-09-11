d25fd74fb0674ac62dbd990babd6f9e816325196
Marco Ojer <marco.ojer@gmail.com>
2017-09-11 19:13:17 +0200

    second trial

--- dummy.py
+++ dummy.py
@@ -3,14 +3,14 @@ from ev3dev.auto import *
 import time
 
 m = Motor(OUTPUT_A)
-u=UltrasonicSensor(INPUT_4)
+u=InfraredSensor(INPUT_4)
 
 print("The maximun velocity is:",m.max_speed)
 for i in range(10):
 	m.run_timed(time_sp=3000, speed_sp=50*i)
 	print ("The relative position is", m.position)
 	print ("the velocity is", 50*i)
-	print("The distances is", u.distance_centimeters)
+	print("The distances is", u.proximity)
 	if i%2==0:
 		screen.draw.rectangle((10,10,60,20), fill='black')
 	else:
